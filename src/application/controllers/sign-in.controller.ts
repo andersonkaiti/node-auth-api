d7bb2f881d9ffe1c06274c67a9ee31b749e6a66e
Anderson Kaiti <anderkaiti@gmail.com>
2026-06-18 23:37:07 -0300

    :sparkles: feat(sign-in): add sign-in controller

---
+++ src/application/controllers/sign-in.controller.ts
@@ -0,0 +1,53 @@
+import { ZodError, z } from 'zod'
+import { InvalidCredentials } from '../errors/invalid-credentials.ts'
+import type {
+  IController,
+  IRequest,
+  IResponse,
+} from '../interfaces/icontroller.ts'
+import type { SignInUseCase } from '../use-cases/sign-in.usecase.ts'
+
+const signInSchema = z.object({
+  email: z.email().min(1),
+  password: z.string().min(8),
+})
+
+export class SignInController implements IController {
+  constructor(private readonly signInUseCase: SignInUseCase) {}
+
+  async handle({ body }: IRequest): Promise<IResponse> {
+    try {
+      const { email, password } = signInSchema.parse(body)
+
+      const { accessToken } = await this.signInUseCase.execute({
+        email,
+        password,
+      })
+
+      return {
+        statusCode: 200,
+        body: {
+          accessToken,
+        },
+      }
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return {
+          statusCode: 400,
+          body: error.issues,
+        }
+      }
+
+      if (error instanceof InvalidCredentials) {
+        return {
+          statusCode: 401,
+          body: {
+            error: 'Invalid credentials',
+          },
+        }
+      }
+
+      throw error
+    }
+  }
+}
