2b1b69f646c02a51be6f8a0f0fb15d38686a9f18
Anderson Kaiti <anderkaiti@gmail.com>
2026-06-19 21:18:42 -0300

    :sparkles: feat(sign-in): add sign-in route

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,5 +1,7 @@
 import express, { type Express, type Request, type Response } from 'express'
+import { SignInController } from '../application/controllers/sign-in.controller.ts'
 import { SignUpController } from '../application/controllers/sign-up.controller.ts'
+import { SignInUseCase } from '../application/use-cases/sign-in.usecase.ts'
 import { SignUpUseCase } from '../application/use-cases/sign-up.usecase.ts'
 import { env } from '../shared/env.ts'
 import { PrismaAccountsRepository } from './database/repositories/prisma-accounts-repository.ts'
@@ -14,6 +16,18 @@ app.post('/sign-up', async (req: Request, res: Response) => {
   const signUpController = new SignUpController(signUpUseCase)
 
   const { statusCode, body } = await signUpController.handle({
+    body: req.body,
+  })
+
+  res.status(statusCode).json(body)
+})
+
+app.post('/sign-in', async (req: Request, res: Response) => {
+  const accountRepository = new PrismaAccountsRepository()
+  const signInUseCase = new SignInUseCase(accountRepository, env.JWT_SECRET)
+  const signInController = new SignInController(signInUseCase)
+
+  const { statusCode, body } = await signInController.handle({
     body: req.body,
   })
 
