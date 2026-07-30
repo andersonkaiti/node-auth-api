2370abb4ca16845fff1c75201ce8dde855d63b68
Anderson Kaiti <anderkaiti@gmail.com>
2026-07-30 16:31:12 -0300

    :sparkles: feat(database): add refresh tokens table and account relation

---
+++ src/infra/database/prisma/migrations/20260730191908_create_table_refresh_tokens/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "refresh_tokens" (
+    "id" UUID NOT NULL,
+    "accountId" UUID NOT NULL,
+    "token" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "refresh_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "refresh_tokens" ADD CONSTRAINT "refresh_tokens_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "accounts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
