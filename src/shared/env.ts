cb29905a2b0998868d767ea2f144e67259250e49
Anderson Kaiti <anderkaiti@gmail.com>
2026-07-31 00:43:44 -0300

    :fire: chore(env): remove REFRESH_TOKEN_SECRET

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -3,7 +3,6 @@ import z from 'zod'
 const envSchema = z.object({
   DATABASE_URL: z.url(),
   JWT_SECRET: z.string(),
-  REFRESH_TOKEN_SECRET: z.string(),
   PORT: z.coerce.number().default(3000),
 })
 
